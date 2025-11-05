b82c8d8bcbaed69b1c2f6c33d77073586cbe2cf8
Shiva Bhattarai <sbhatta9@kent.edu>
2025-11-04 21:21:59 -0600

    Initial commit: ML deployment project structure

--- app/inference.py
+++ app/inference.py
@@ -1,7 +1,13 @@
+import pickle
 import os
 
 class ModelWrapper:
     def __init__(self):
-        model_path = "/app/model/diabetes_prediction_model.pkl"  # full path in container
+        # Correct path inside Lambda container
+        model_path = os.path.join(os.environ.get("LAMBDA_TASK_ROOT", "/var/task"), "model", "diabetes_prediction_model.pkl")
         with open(model_path, "rb") as f:
             self.model = pickle.load(f)
+
+    def predict(self, features):
+        # Make sure features is in a list-of-lists shape if needed
+        return self.model.predict([features])[0]
